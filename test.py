91420e0a76d4ce64b96380e209e21d6b1ec7b997
Manivannan Selvaraj <maselvaraj@users.noreply.github.com>
2020-05-28 15:11:49 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-2.0, 0.0, 2.0, 4.0, 6.0, 8.0], dtype=float)
+
+model.fit(xs, ys, epochs=1000)
+
+print(model.predict([20.0]))
